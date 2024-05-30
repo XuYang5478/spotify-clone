ffc6d06424b9b15a749f741e28307a01677e13d4
XuYang5478 <1826712273@qq.com>
2024-05-30 21:20:44 +0800

    add auth UI
    Change-Id: Ie743c4dfa83bc8fd2a69329624f0e3feffb28556

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { twMerge } from "tailwind-merge";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -13,6 +14,7 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
   const router = useRouter();
+  const authModal = useAuthModal();
 
   const handleLogout = () => {
     // 处理账号注销
@@ -60,12 +62,14 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
         <div className="flex justify-between items-center gap-x-4">
           <>
             <div>
-              <Button className="bg-transparent text-neutral-300 font-medium">
+              <Button className="bg-transparent text-neutral-300 font-medium"
+                onClick={authModal.onOpen}>
                 Sign Up
               </Button>
             </div>
             <div>
-              <Button className="bg-white">
+              <Button className="bg-white"
+                onClick={authModal.onOpen}>
                 Log In
               </Button>
             </div>
