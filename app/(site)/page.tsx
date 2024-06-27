0337205b350462a63b3ac9e2dcd6876bef0ae402
XuYang5478 <1826712273@qq.com>
2024-06-27 21:28:58 +0800

    partly player
    Change-Id: I928b726fea47f9f8f9669f1b5e92074975067b1d

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -13,7 +13,7 @@ export default async function Home() {
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">
-            SomeHeader
+            Welcome back
           </h1>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
